c517cce73df66b670410c73a5b317775eb1d1207
takkiemon <takkie91@hotmail.com>
2019-12-04 03:04:00 +0100

    wip: creating playercontroller script with movement between lanes

---
+++ EndlessRunnerCI/Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Input;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObjects[] lanes;
+    public int currentLane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLane = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown()) ;
+    }
+}
