fbea7c6a0fe31c236e8cf5779ccd969df289cb3d
takkiemon <takkie91@hotmail.com>
2019-12-04 15:55:21 +0100

    implement points and showing score in UI

--- EndlessRunnerCI/Assets/Scripts/PlayerController.cs
+++ EndlessRunnerCI/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public GameObject[] lanes;
     public int currentLane;
+    public int points;
     public bool moving;
     public float moveTimer;
     public float timeToMove;
+    public Text scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
+        points = 0;
         currentLane = 2;
         moveTimer = 0f;
-        timeToMove = 3f;
+        timeToMove = 1.5f;
     }
 
     // Update is called once per frame
@@ -23,21 +27,23 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            if (currentLane > 1)
+            if (!moving && currentLane > 1)
             {
                 currentLane--;
                 moving = true;
                 moveTimer = 0f;
             }
+            //UpdatePoints(++points);
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if (currentLane < lanes.Length)
+            if (!moving && currentLane < lanes.Length)
             {
                 currentLane++;
                 moving = true;
                 moveTimer = 0f;
             }
+            //UpdatePoints(++points);
         }
 
         if (moving)
@@ -55,5 +61,11 @@ public class PlayerController : MonoBehaviour
             moveTimer = 1f;
         }
         transform.position = Vector3.Lerp(startingPosition, finishingPosition, moveTimer);
+    }
+
+    public void UpdatePoints(int points)
+    {
+        this.points = points;
+        scoreText.text = points.ToString();
     }
 }
