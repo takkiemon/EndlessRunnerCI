86569ce55d3dc1df7c7ff67c2626a225d6c347b4
takkiemon <takkie91@hotmail.com>
2019-12-04 17:40:44 +0100

    added conveyorbelt object that controls the moving things created gold coins and have them move under the control of the conveyorbelt object

---
+++ EndlessRunnerCI/Assets/Scripts/ConveyorBeltBehavior.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBeltBehavior : MonoBehaviour
+{
+    public float movingSpeed;
+    public int spawnType;
+    public GameObject[] itemLanes;
+    public List<GameObject> itemsOnTheBelt;
+    public List<GameObject> inactiveItems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnSomething", 1f, .75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(GameObject beltObject in itemsOnTheBelt)
+        {
+            beltObject.transform.position = new Vector3(beltObject.transform.position.x, beltObject.transform.position.y, beltObject.transform.position.z - movingSpeed);
+        }
+    }
+
+    public void SpawnSomething()
+    {
+        spawnType = Random.Range(1, 3);
+        int lane = Random.Range(1, 4);
+        switch(spawnType)
+        {
+            case 0:
+                //do nothing;
+                break;
+            case 1:
+                SpawnCoin(lane);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void SpawnCoin(int lane)
+    {
+        foreach(GameObject currentObject in inactiveItems)
+        {
+            if (currentObject.GetComponent<CoinBehavior>())
+            {
+                itemsOnTheBelt.Add(currentObject);
+                inactiveItems.Remove(currentObject);
+                currentObject.transform.position = itemLanes[lane - 1].transform.position;
+                return;
+            }
+        }
+
+        foreach(GameObject currentObject in itemsOnTheBelt)
+        {
+            if (currentObject.GetComponent<CoinBehavior>())
+            {
+                currentObject.transform.position = itemLanes[lane - 1].transform.position;
+                return;
+            }
+        }
+        Debug.Log("the inactiveItems and itemsOnTheBelt lists both do not contain any objects with the 'Coinbehavior.cs' component: there are no coins in the game or the 'SpawnCoin' function doesn't work properly");
+    }
+}
---
+++ EndlessRunnerCI/Assets/Scripts/ItemCatcherBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCatcherBehavior : MonoBehaviour
+{
+    public ConveyorBeltBehavior conveyorBelt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!conveyorBelt.inactiveItems.Contains(other.gameObject))
+        {
+            conveyorBelt.inactiveItems.Add(other.gameObject);
+        }
+        if (conveyorBelt.itemsOnTheBelt.Contains(other.gameObject))
+        {
+            conveyorBelt.itemsOnTheBelt.Remove(other.gameObject);
+        }
+        other.gameObject.transform.position = new Vector3(other.gameObject.transform.position.x, other.gameObject.transform.position.y + transform.localScale.y + transform.position.y + 2, other.gameObject.transform.position.z - transform.localScale.z + transform.position.z - 2);
+    }
+}
